582bc7dd32b8595a5f010b26edeeaf315e814463
esteban-florez <eflorez077@gmail.com>
2025-11-21 09:57:39 -0400

    ✨FEAT: Añadir archivo real en FileSeeder

--- database/seeders/DocumentSeeder.php
+++ database/seeders/DocumentSeeder.php
@@ -7,26 +7,33 @@ use Illuminate\Database\Seeder;
 use App\Models\Document;
 use App\Models\Project;
 use App\Models\Equipment;
+use App\Models\Part;
 
 class DocumentSeeder extends Seeder
 {
     public function run(): void
     {
-        $project = Project::first();
         $equipment = Equipment::first();
+        $part = Part::first();
+        $project = Project::first();
 
         $documents = [
             [
                 'name' => 'Plano de conjunto',
                 'category' => Category::Blueprint->value,
-                'documentable_type' => Project::class,
-                'documentable_id' => $project->id
+                'documentable_type' => Part::class,
+                'documentable_id' => $part->id
             ],
             [
                 'name' => 'Manual de operación',
                 'category' => Category::Manual->value,
                 'documentable_type' => Equipment::class,
                 'documentable_id' => $equipment->id
+            ],
+            [
+                'name' => 'Propuesta realizada',
+                'documentable_type' => Project::class,
+                'documentable_id' => $project->id
             ],
         ];
 
--- database/seeders/FileSeeder.php
+++ database/seeders/FileSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\Document;
 use App\Models\File;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Storage;
 
 class FileSeeder extends Seeder
 {
@@ -13,25 +14,60 @@ class FileSeeder extends Seeder
      */
     public function run(): void
     {
-        $documents = Document::all();
+        $documents = Document::oldest()->get();
 
         $files = [
             [
-                'path' => 'docs/plano_conjunto.pdf',
+                'path' => 'docs/sample.pdf',
                 'mime' => 'application/pdf',
                 'version' => 1,
                 'document_id' => $documents[0]->id
             ],
             [
-                'path' => 'docs/manual_operacion.pdf',
+                'path' => 'docs/sample.pdf',
                 'mime' => 'application/pdf',
                 'version' => 1,
                 'document_id' => $documents[1]->id
+            ],
+            [
+                'path' => 'docs/sample.pdf',
+                'mime' => 'application/pdf',
+                'version' => 1,
+                'document_id' => $documents[2]->id
             ],
         ];
 
         foreach ($files as $file) {
             File::create($file);
+        }
+
+        if (Storage::missing('sample.pdf')) return;
+
+        $files = File::with(['document', 'document.documentable'])
+            ->get();
+
+        foreach ($files as $file) {
+            $document = $file->document;
+
+            $folder = model_to_spanish(
+                model: $document->documentable_type,
+                plural: true
+            );
+
+            $segments = collect([$folder, $document->documentable->name]);
+            $category = $document->category;
+            
+            if ($category) {
+                $segments->push($category->value);
+            }
+
+            $name = str($file->document->name)
+                ->append(" - V1.pdf");
+            $segments->push($name);
+
+            $path = $segments->join('/');
+
+            Storage::copy('sample.pdf', $path);
         }
     }
 }
