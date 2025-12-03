78f48107f22d6c8f36fb4d3e7468743b1a15a40a
JesusMiguel03 <jmdefreitas10@gmail.com>
2025-12-02 20:46:44 -0400

    ♻️FIX: Corregir texto de bitácora en base al modelo

--- app/Traits/HasActivityLog.php
+++ app/Traits/HasActivityLog.php
@@ -2,6 +2,7 @@
 
 namespace App\Traits;
 
+use Illuminate\Support\Str;
 use Spatie\Activitylog\LogOptions;
 
 trait HasActivityLog
@@ -12,8 +13,7 @@ trait HasActivityLog
     public function getActivitylogOptions(): LogOptions
     {
         $spanishPlural = model_to_spanish($this::class, plural: true);
-        $spanishSingular = model_to_spanish($this::class);
-
+        $spanishSingular = strtolower(model_to_spanish($this::class));
         $fieldsToIgnore = ['id', 'created_at', 'updated_at'];
 
         if (property_exists($this, 'activityIgnoredAttributes')) {
@@ -21,14 +21,18 @@ trait HasActivityLog
             $fieldsToIgnore = array_unique($fieldsToIgnore);
         }
 
+        $article = $this::class === \App\Models\Activity::class
+            ? 'Una'
+            : 'Un';
+
         return LogOptions::defaults()
             ->useLogName($spanishPlural)
             ->logAll()
             ->logExcept($fieldsToIgnore)
             ->dontSubmitEmptyLogs()
-            ->setDescriptionForEvent(function (string $eventName) use ($spanishSingular) {
-                $translatedEvent = translate_activity_verb($eventName);
-                return "Un {$spanishSingular} fue {$translatedEvent} en el sistema.";
+            ->setDescriptionForEvent(function (string $eventName) use ($article, $spanishSingular) {
+                $translatedEvent = Str::replaceLast('o', 'a', translate_activity_verb($eventName));
+                return "{$article} {$spanishSingular} fue {$translatedEvent} en el sistema.";
             })
             ->logOnlyDirty();
     }
