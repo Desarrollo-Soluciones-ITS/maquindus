0d97ddb49da3962dc2fef5a5a5ff18853861fc54
DavidGonzalez321 <davidm.gonzalezt@gmail.com>
2026-06-10 14:39:55 -0400

    feat(routes): add folder open route and fix indentation
    Add a new route for opening a folder via FolderController, protected by 'auth' and 'verified' middleware. Also fix indentation of the existing files.preview route.

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,15 @@
 <?php
 
 use App\Http\Controllers\PreviewFileController;
+use App\Http\Controllers\FolderController;
 use Illuminate\Support\Facades\Route;
 
 Route::redirect('/', '/dashboard');
 
 Route::middleware(['permission.any:documents.show_file,files.show_file'])
-	->get('/files/{file}/preview', PreviewFileController::class)
-	->name('files.preview');
+        ->get('/files/{file}/preview', PreviewFileController::class)
+        ->name('files.preview');
+
+Route::middleware(['auth', 'verified'])->group(function () {
+    Route::get('/folder/open', [FolderController::class, 'open'])->name('folder.open');
+});
